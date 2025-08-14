95dff7853024c925f8587bc88cdbb2a3ad2efe06
Zachary Hamida <zac.hamida@gmail.com>
2025-08-13 20:42:22 -0400

    deploy apps

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/zacharyhamida/Desktop/Career/zachamida.github.io/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Configure for development
-  images: {
-    domains: ['zachamida.github.io'], // Allow images from your domain
-  }
-};
-
-module.exports = nextConfig;
